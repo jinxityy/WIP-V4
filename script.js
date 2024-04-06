b9c0a64770d169f352a5c53cca3d49a86d0daa2c
Cyber Panel <prollhouse4@gmail.com>
2024-04-06 00:00:10 +0000

    Auto commit by CyberPanel Daily cron on 04-06-2024_00-00-09

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("revealButton").addEventListener("click", function() {
-  var hiddenElement = document.getElementById("hiddenElement");
-  if (hiddenElement.style.display === "none") {
-    hiddenElement.style.display = "block"; // Show the element
-  } else {
-    hiddenElement.style.display = "none"; // Hide the element
-  }
-});
